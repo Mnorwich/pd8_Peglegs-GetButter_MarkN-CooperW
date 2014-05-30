c42cb909663f7fe2bd4ca2b47010a034d6f124ef
MarkNorwich <mark.norwich@cslab3-5.stuy.edu>
2014-05-29 21:57:57 -0400

    updated devlog, driver started

---
+++ Driver.java
@@ -0,0 +1,23 @@
+import java.io.*; 
+import java.util.*; 
+import java.util.Scanner.*; 
+
+public class Driver{
+    
+    private String name; 
+    private String rescuee; 
+    
+    public void main(String name, String rescuee){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Hello there warrior! Input your name");
+	name = sc.findInLine(); 
+	while (name.size() == 0){
+	    System.out.println(");
+	    name = sc.findInLine(); 
+	}
+	System.out.println("Great " + name "
+    
+    
+	    
+	
+    
